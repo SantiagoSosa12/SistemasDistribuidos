7b4eb212d50d8c41dc31c5fce9882772eab7890d
SantiagoSosaReyes <santiago.sosa@uptc.edu.co>
2020-11-18 18:13:12 -0500

    Entrada y Salida de Pokemones

--- index.js
+++ index.js
@@ -3,29 +3,28 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.use(express.json())
+let pokemons = ["Charizad" , "Bulbasur"]
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.post('/', (req, res) => {
+  let pokemonsTS = ''
+  pokemons.forEach(function(elemento, array){
+    pokemonsTS += ' ' + elemento
+    
+  })
+  res.send(pokemonsTS)
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-app.post('/Pokemones/', (req,res) => {
-    console.log(req.body.name)
-    res.send('Usted esta enviando pokemons')
+app.post('/RegistrarPokemones/:pokemon', (req,res) => {
+    pokemons.push(req.params.pokemon)
+    console.log(pokemons[pokemons.length - 1])//Con esto verificamos 
+    //que el pokemon se haya registrado
+    res.send('Usted registro un pokemon')
 })
 
-app.post('/EquiposDeFutbol/', (req,res) => {
-    console.log(req.body.name)
-    res.send('Usted esta enviando Equipos de Futbol')
-})
-
-app.post('/ClasicosDelRock/', (req,res) => {
-    console.log(req.body.name)
-    res.send('Usted esta enviando Equipos de Futbol')
-})
 
 //{
 //  "pokemons":[

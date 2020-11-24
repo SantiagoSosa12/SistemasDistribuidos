deacc85c0088e08dbf98748e18a2450d98cf464e
SantiagoSosaReyes <santiago.sosa@uptc.edu.co>
2020-11-24 10:24:36 -0500

    Se pueden registrar pokemones :)

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
+const cors = require('cors')
+const port = 8080
+
+app.use(cors())
 app.use(express.json())
+
 let pokemons = ["Charizad" , "Bulbasur"]
 
-app.post('/', (req, res) => {
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+
+
+app.get('/pokemons', (req, res) => {
   let pokemonsTS = ''
   pokemons.forEach(function(elemento, array){
     pokemonsTS += ' ' + elemento
@@ -14,17 +23,16 @@ app.post('/', (req, res) => {
   res.send(pokemonsTS)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
-app.post('/RegistrarPokemones/:pokemon', (req,res) => {
+app.get('/RegistrarPokemones/:pokemon', (req,res) => {
     pokemons.push(req.params.pokemon)
     console.log(pokemons[pokemons.length - 1])//Con esto verificamos 
     //que el pokemon se haya registrado
     res.send('Usted registro un pokemon')
 })
 
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
 
 //{
 //  "pokemons":[

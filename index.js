d58f0e46863e51bf6bc895f82caea8d6b81d9ef2
SantiagoSosaReyes <santiago.sosa@uptc.edu.co>
2020-12-01 14:46:32 -0500

    lee archvo estado de sevidores

--- index.js
+++ index.js
@@ -1,38 +1,22 @@
 
+
 const express = require('express')
 const app = express()
-const cors = require('cors')
-const port = 8080
-
-app.use(cors())
-app.use(express.json())
-
-let pokemons = ["Charizad" , "Bulbasur"]
-
-app.get('/', function(req, res) {
-  res.send('hello world');
-});
+const port = 3000
+const fs= require('fs');
 
 
-app.get('/pokemons', (req, res) => {
-  let pokemonsTS = ''
-  pokemons.forEach(function(elemento, array){
-    pokemonsTS += ' ' + elemento
-    
-  })
-  res.send(pokemonsTS)
-})
-
-app.get('/RegistrarPokemones/:pokemon', (req,res) => {
-    pokemons.push(req.params.pokemon)
-    console.log(pokemons[pokemons.length - 1])//Con esto verificamos 
-    //que el pokemon se haya registrado
-    res.send('Usted registro un pokemon')
+app.get('/', (req, res) => {
+  let archivo = fs.readFileSync('/serverStatus/logger.txt', 'utf-8');
+  console.log(archivo);
+  res.send(archivo);
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
 
 //{
 //  "pokemons":[

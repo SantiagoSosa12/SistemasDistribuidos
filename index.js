7d70cc7087cde18ddaff3f1b82c10f8aaa2c3f90
SantiagoSosaReyes <santiago.sosa@uptc.edu.co>
2020-12-01 17:22:13 -0500

    comentareado

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 
-var fs = require('fs');
-var array = fs.readFileSync('/home/serverone/serverStatus/logger.txt').toString().split("\n");
-for(i in array) {
-  console.log(array[i]);
-}
 
-console.log(array[array.length - 2] + " Ultima linea :'v");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  var fs = require('fs');
+  var array = fs.readFileSync('/home/serverone/serverStatus/logger.txt').toString().split("\n");
+  res.send(array[array.length - 2]);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+//var fs = require('fs');
+//var array = fs.readFileSync('/home/serverone/serverStatus/logger.txt').toString().split("\n");
+//for(i in array) {
+//  console.log(array[i]);
+//}
+
+//console.log(array[array.length - 2] + " Ultima linea :'v");

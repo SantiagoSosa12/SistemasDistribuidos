35b54f1a34f5e5306ab50c4374082c1c14359eb2
SantiagoSosaReyes <santiago.sosa@uptc.edu.co>
2020-12-01 19:31:37 -0500

    se anadio cors

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 
 const express = require('express')
 const app = express()
+const cors = require("cors")
 const port = 3000
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   var fs = require('fs');
